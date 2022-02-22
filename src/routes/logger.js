eb74de2c26e85ee8cfec5ae9db0e6f462a620d14
prasadjadh <prasadljo7@gmail.com>
2022-02-22 14:56:15 +0530

    sai

---
+++ src/routes/logger.js
@@ -0,0 +1,10 @@
+let pro ="www.google.com"
+function fuc(massege){
+    console.log(massege)
+}
+
+module.exports.endpoint = pro
+module.exports.test= fuc
+
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,12 @@
+const obj =  require ('./logger')
 const express = require('express');
 
 const router = express.Router();
 
+
 router.get('/test-me', function (req, res) {
+    obj.test('HI')
+    console.log(obj.pro)
     res.send('My first ever api!')
 });
 
