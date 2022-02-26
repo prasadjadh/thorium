1a8bbabeeeabf1d5cdfc2eef07fa6ea1e983b3b4
prasadjadh <prasadljo7@gmail.com>
2022-02-26 23:56:09 +0530

    api1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,65 +1,28 @@
 const express = require('express');
 const router = express.Router();
+let players = [];
+
+router.post ("/players",function(req,res){
+   
+    let player = req.body;
+    let playerName = player.name
+    for(let i = 0; i < players.length; i++){
+        if(players[i].name == playerName){
+            res.send('player already exists')
+        }
+    }
+    players.push(player);
+    console.log ("Here is the player array",players);
+    res.send(players);
+}) ;  
 
 router.get('/students/:name', function(req, res) {
     let studentName = req.params.name
     console.log(studentName)
     res.send(studentName)
+
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
-
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
 
 
 
@@ -70,4 +33,4 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
-module.exports = router;
+module.exports = router ;
