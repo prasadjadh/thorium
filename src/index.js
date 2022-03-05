ea841df68c04d0463131459673219874e497b306
Manish-162403 <naamcool420@gmail.com>
2022-03-05 10:36:16 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment= require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(request.mw({attributeName:'myIp'}))
 
 
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
@@ -16,6 +19,14 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 
 app.use('/', route);
 
+//let c= "122.152.18.3" 
+//
+// app.use(function(req,res,next){
+// let ip = req.myIp
+//   console.log(moment().format()+" "+ip+" "+req.originalUrl)
+  
+//     next()
+// })
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,39 +3,68 @@ const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
+const app = express();
+const moment= require('moment')
+const request= require('request-ip')
+app.use(request.mw({attributeName:'myIp'}))
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+app.use(function(req,res,next){
+    let ip = req.myIp
+      console.log(moment().format()+" "+ip+" "+req.originalUrl)
+      
+        next()
+    })
 
-router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/test-me", BookController.test_me)
 
-router.post("/createBook", BookController.createBook  )
+router.get("/one", BookController.one)
 
-router.get("/getBooksData", BookController.getBooksData)
-
-router.post("/updateBooks", BookController.updateBooks)
-router.post("/deleteBooks", BookController.deleteBooks)
-
-//MOMENT JS
-const moment = require('moment');
-router.get("/dateManipulations", function (req, res) {
-    
-    // const today = moment();
-    // let x= today.add(10, "days")
-
-    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
-    // console.log(validOrNot)
-    
-    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
-    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
-
-    let x= dateB.diff(dateA, "days")
-    console.log(x)
-
-    res.send({ msg: "all good"})
-})
+router.get("/test", BookController.test)
 
 module.exports = router;
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
+
+// router.post("/createBook", BookController.createBook  )
+
+// router.get("/getBooksData", BookController.getBooksData)
+
+// router.post("/updateBooks", BookController.updateBooks)
+// router.post("/deleteBooks", BookController.deleteBooks)
+
+// //MOMENT JS
+// const moment = require('moment');
+// router.get("/dateManipulations", function (req, res) {
+    
+//     // const today = moment();
+//     // let x= today.add(10, "days")
+
+//     // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
+//     // console.log(validOrNot)
+    
+//     const dateA = moment('01-01-1900', 'DD-MM-YYYY');
+//     const dateB = moment('01-01-2000', 'DD-MM-YYYY');
+
+//     let x= dateB.diff(dateA, "days")
+//     console.log(x)
+
+//     res.send({ msg: "all good"})
+// })
+
